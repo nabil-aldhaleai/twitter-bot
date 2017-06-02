8b19edc0229d3a1b6541da5b46f0ccb9df664406
Jamie Strachan <jstrachan@infotech.com>
2017-06-01 22:17:19 -0400

    setting up more query options

--- twitterModel.php
+++ twitterModel.php
@@ -21,11 +21,15 @@
     #$res = $mysqli->query("SELECT * FROM Concerts WHERE openers is not null AND link is not null ORDER BY rand() LIMIT 1");
 
     # pick an event that happened this month
-    #$today = new DateTime("now", new DateTimeZone('America/Toronto'));
-    #$res = $mysqli->query("SELECT * FROM Concerts WHERE month(date) = " . $today->format('m') . " ORDER BY rand() LIMIT 1");
+    $today = new DateTime("now", new DateTimeZone('America/Toronto'));
+    $res = $mysqli->query("SELECT * FROM Concerts WHERE month(date) = " . $today->format('m') . " ORDER BY rand() LIMIT 1");
 
     # pick an event that happened in an arbritary month
-    $res = $mysqli->query("SELECT * FROM Concerts WHERE month(date) = 11 ORDER BY rand() LIMIT 1");
+    #$res = $mysqli->query("SELECT * FROM Concerts WHERE month(date) = 2 ORDER BY rand() LIMIT 1");
+
+    # tweet a specific event
+    #$res = $mysqli->query("SELECT * FROM Concerts WHERE id = 2 LIMIT 1");
+
 
     # pick an event that has an image
     #$res = $mysqli->query("SELECT * FROM Concerts WHERE image is not null ORDER BY rand() LIMIT 1");
